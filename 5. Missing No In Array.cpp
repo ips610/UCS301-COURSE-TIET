6db86ab2c1ddaca48399a53717cb3354a13877d7
Ishpuneet Singh <ishpuneetsingh6@gmail.com>
2023-08-09 15:30:47 +0530

    missing no in array all cases satisfied

--- 5. Missing No In Array.cpp
+++ 5. Missing No In Array.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 using namespace std;
+
 void missingNo(int arr[], int size){
+
     int s=0;
     int e=size-1;
     int mid=s+(e-s)/2;
+    int check=0;
 
     while(s<=e){
         if (arr[mid]-mid>1){
             e=mid-1;
+
         }
         else{
             s=mid+1;
+            check=1;
         }
         mid=s+(e-s)/2;
 
     }
-    cout<<mid+1<<endl;
+    cout<<mid+1+check<<endl;
+
 }
 int main(){
-        int arr[10]={1,2,4,6,7,9,10,11,12,13};
-        int size=10;
+
+        int arr[13]={1,2,3,4,5,6,8,10,11,13,15,17};
+        int size=13;
         missingNo(arr,5);
+
 }
